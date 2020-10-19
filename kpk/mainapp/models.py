489ee08227118989fe21dcf98f909d48407150af
Jo <carwash999@yandex.ru>
2020-10-19 10:11:05 +0500

    first model

--- kpk/mainapp/models.py
+++ kpk/mainapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class SubjectCategory(models.Model):
+    name = models.CharField(max_length=128)
+    desc = models.TextField(blank=True)
