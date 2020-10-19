77b34afb72440ddc8f40e43298b5f3f8146b65ea
Jo <carwash999@yandex.ru>
2020-10-19 13:10:39 +0500

    db content in template

--- kpk/mainapp/views.py
+++ kpk/mainapp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
+from mainapp.models import SubjectCategory
+
 
 def index(request):
     return render(request, 'mainapp/index.html')
 
 
 def catalog(request):
-    return render(request, 'mainapp/catalog.html')
+    categories = SubjectCategory.objects.all()
+    context = {
+        'categories': categories
+    }
+
+    return render(request, 'mainapp/catalog.html', context)
 
 
 def basket(request):
