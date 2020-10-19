1fc039698fa606631e8338d13758e37f0dd372b0
Jo <carwash999@yandex.ru>
2020-10-19 11:00:59 +0500

    Many-To-One

--- kpk/mainapp/admin.py
+++ kpk/mainapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from mainapp.models import SubjectCategory
+from mainapp.models import SubjectCategory, Course
 
 admin.site.register(SubjectCategory)
+admin.site.register(Course)
--- kpk/mainapp/models.py
+++ kpk/mainapp/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class SubjectCategory(models.Model):
     name = models.CharField(max_length=128)
     desc = models.TextField(blank=True)
+
+
+class Course(models.Model):
+    category = models.ForeignKey(SubjectCategory,
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    desc = models.TextField(blank=True)
+    hours = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+# 5 category
+# 8 items
+# 5 + 40 = 45 items
