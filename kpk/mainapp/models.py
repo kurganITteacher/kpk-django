9e29b0d836f09dbee106ccce834c363235925b22
Jo <carwash999@yandex.ru>
2020-10-24 10:41:10 +0500

    choice field

--- kpk/mainapp/models.py
+++ kpk/mainapp/models.py
@@ -14,12 +14,26 @@ class SubjectCategory(models.Model):
 
 
 class Course(models.Model):
+    EASY = 'e'
+    MEDIUM = 'm'
+    HARD = 'h'
+    LEVEL_CHOICES = (
+        (EASY, 'легкий'),
+        (MEDIUM, 'средний'),
+        (HARD, 'продвинутый'),
+    )
+
     category = models.ForeignKey(SubjectCategory,
                                  on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     desc = models.TextField(blank=True)
     hours = models.IntegerField(default=0)
+    level = models.CharField(max_length=1, choices=LEVEL_CHOICES, default=EASY)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
