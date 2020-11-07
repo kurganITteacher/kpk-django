abb078c52dd403889a76b9c519091d0e87ab1040
Jo <carwash999@yandex.ru>
2020-11-07 15:36:47 +0500

    register

--- kpk/authapp/forms.py
+++ kpk/authapp/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 
 
 class LoginForm(AuthenticationForm):
@@ -6,3 +7,16 @@ class LoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
         for name, item in self.fields.items():
             item.widget.attrs['class'] = f'form-control {name}'
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name',
+                  'password1', 'password2', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, item in self.fields.items():
+            item.widget.attrs['class'] = f'form-control {name}'
+            item.help_text = ''
--- kpk/authapp/views.py
+++ kpk/authapp/views.py
@@ -1,8 +1,9 @@
 import django.contrib.auth as auth
-from authapp.forms import LoginForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+
+from authapp.forms import LoginForm, RegisterForm
 
 
 def login(request):
@@ -10,7 +11,6 @@ def login(request):
         form = LoginForm(data=request.POST)
         if form.is_valid():
             auth.login(request, form.get_user())
-            # return HttpResponseRedirect('/')
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = LoginForm()
@@ -25,3 +25,20 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('main:index'))
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        form = RegisterForm()
+
+    context = {
+        'page_title': 'регистрация',
+        'form': form,
+    }
+    return render(request, 'authapp/register.html', context)
+
