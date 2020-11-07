6ee99483cb0a54657e4338208f9940b95bf8cbc3
Jo <carwash999@yandex.ru>
2020-11-07 13:44:42 +0500

    bootstrap

--- kpk/authapp/forms.py
+++ kpk/authapp/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.models import User
 
 
 class LoginForm(AuthenticationForm):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, item in self.fields.items():
+            item.widget.attrs['class'] = 'form-control'
--- kpk/authapp/views.py
+++ kpk/authapp/views.py
@@ -1,11 +1,9 @@
-from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
 
 from authapp.forms import LoginForm
 
 
 def login(request):
-    # form = AuthenticationForm()
     form = LoginForm()
 
     context = {
