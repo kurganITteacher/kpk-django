8649d539b7db71520b228a650a7ff657b75ae3b0
Jo <carwash999@yandex.ru>
2020-11-07 12:35:06 +0500

    login page

---
+++ kpk/authapp/forms.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class LoginForm(AuthenticationForm):
+    pass
--- kpk/authapp/views.py
+++ kpk/authapp/views.py
@@ -1,5 +1,15 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
+
+from authapp.forms import LoginForm
 
 
 def login(request):
-    pass
+    # form = AuthenticationForm()
+    form = LoginForm()
+
+    context = {
+        'page_title': 'авторизация',
+        'form': form,
+    }
+    return render(request, 'authapp/login.html', context)
