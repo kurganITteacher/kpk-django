b17f0a7ac1fd8b11186d8a35e604a299962fda2d
Jo <carwash999@yandex.ru>
2020-12-19 10:47:09 +0500

    CBV

--- kpk/mainapp/urls.py
+++ kpk/mainapp/urls.py
@@ -5,7 +5,9 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.index, name='index'),
-    path('catalog/', mainapp.catalog, name='catalog'),
+
+    # path('catalog/', mainapp.catalog, name='catalog'),
+    path('catalog/', mainapp.CatalogListView.as_view(), name='catalog'),
 
     path('catalog/category/<int:category_pk>/', mainapp.catalog_section, name='catalog_section'),
     path('catalog/course/<int:course_pk>/', mainapp.course_page, name='course_page'),
--- kpk/mainapp/views.py
+++ kpk/mainapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from mainapp.models import SubjectCategory, Course
 
@@ -7,16 +8,72 @@ def index(request):
     return render(request, 'mainapp/index.html')
 
 
-def catalog(request):
-    categories = SubjectCategory.objects.all()
-    context = {
-        'categories': categories,
-        'page_title': 'каталог'
-    }
+# # FBV - Function Based Views
+# def catalog(request):
+#     categories = SubjectCategory.objects.all()
+#     categories = SubjectCategory.objects.filter(is_active=True)
+#     context = {
+#         'categories': categories,
+#         'page_title': 'каталог'
+#     }
+#
+#     return render(request, 'mainapp/catalog.html', context)
+pass
 
-    return render(request, 'mainapp/catalog.html', context)
+# Class Based Views
+# tmplate name -> class name + suffix
+# tmplate name -> subjectcategory_list
+# mainapp/subjectcategory_list.html
+# class CatalogListView(ListView):
+#     model = SubjectCategory
+#     # context_object_name = 'categories'
+#     # template_name = 'mainapp/catalog.html'
+#
+#     # def get_context_data(self, *, object_list=None, **kwargs):
+#     #     context = super().get_context_data(object_list, **kwargs)
+#     #     context['second_list'] = pass
+#     #     return context
+#
+#     # def get_queryset(self):
+#     #     return self.model.objects.filter(is_active=True)
+pass
 
 
+# class CatalogListView(ListView):
+#     model = SubjectCategory
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(object_list=object_list, **kwargs)
+#         context['page_title'] = 'каталог'
+#         # print('context', context)
+#         return context
+
+class PageTitleMixin:
+    page_title = ''
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['page_title'] = self.page_title
+        return context
+
+
+class CatalogListView(PageTitleMixin, ListView):
+    model = SubjectCategory
+    page_title = 'каталог'
+
+
+# 'locationsapp/locations_list.html'
+# class LocationsListView(ListView):
+#     model = Locations
+#     page_title = 'локации'
+#
+#
+# class ManagersListView(PageTitleMixin, ListView):
+#     model = Managers
+#     page_title = 'менеджеры'
+
+
+# DetailView -> object, category_pk -> pk
 def catalog_section(request, category_pk):
     courses = Course.objects.filter(category_id=category_pk)
     context = {
